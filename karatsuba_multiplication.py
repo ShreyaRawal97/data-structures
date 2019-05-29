9f1468293ae5dcb57e1fad0936dfa5d4463869f5
ShreyaRawal97 <50499340+ShreyaRawal97@users.noreply.github.com>
2019-05-29 15:54:34 -0500

    Update karatsuba_multiplication.py

--- karatsuba_multiplication.py
+++ karatsuba_multiplication.py
@@ -1,22 +1,9 @@
 """
-In this programming assignment you will implement one or more of the integer multiplication algorithms described in lecture.
-
-To get the most out of this assignment, your program should restrict itself to multiplying only pairs of single-digit numbers. You can implement the grade-school algorithm if you want, but to get the most out of the assignment you'll want to implement recursive integer multiplication and/or Karatsuba's algorithm.
-
-So: what's the product of the following two 64-digit numbers?
+What's the product of the following two 64-digit numbers?
 
 3141592653589793238462643383279502884197169399375105820974944592
 
 2718281828459045235360287471352662497757247093699959574966967627
-
-[TIP: before submitting, first test the correctness of your program on some small test cases of your own devising. Then post your best test cases to the discussion forums to help your fellow students!]
-
-[Food for thought: the number of digits in each input number is a power of 2. Does this make your life easier? Does it depend on which algorithm you're implementing?]
-
-The numeric answer should be typed in the space below. So if your answer is 1198233847, then just type 1198233847 in the space provided without any space / commas / any other punctuation marks.
-
-(We do not require you to submit your code, so feel free to use any programming language you want --- just type the final numeric answer in the following space.)
-
 """
 
 import time #imported the time library so that we could compare how much time each form of multiplication takes
@@ -30,9 +17,9 @@ def zeroPad(numberString, zeros, left = True):
     #start for loop i from zero to the number of zeroes we need
     for i in range(zeros):
         if left:
-            numberString = '0' + numberString
+            numberString = '0' + numberString #adds zeroes to the left
         else:
-            numberString = numberString + '0'
+            numberString = numberString + '0' #adds zeroes to the right
     return numberString
 
 def karatsubaMultiplication(x ,y):
@@ -42,30 +29,31 @@ def karatsubaMultiplication(x ,y):
     y = str(y) #change y to a string
     #base case for recursion
     if len(x) == 1 and len(y) == 1:
-        return int(x) * int(y)
+        return int(x) * int(y) #use grade school multiplication if the the two numbers are one digit numbers
     if len(x) < len(y):
-        x = zeroPad(x, len(y) - len(x))
+        x = zeroPad(x, len(y) - len(x)) #if the numbers are of unequeal length, we pad them with zeroes on the left accordingly
     elif len(y) < len(x):
-        y = zeroPad(y, len(x) - len(y))
-    n = len(x)
-    j = n//2
+        y = zeroPad(y, len(x) - len(y)) #if the numbers are of unequeal length, we pad them with zeroes on the left accordingly
+    n = len(x) #length of the number after padding them with zeroes (so now both numbers are equal)
+    j = n//2 #get the half length (we only want the integer division here, not decimal part)
     #for odd digit integers
     if (n % 2) != 0:
         j += 1
-    BZeroPadding = n - j
-    AZeroPadding = BZeroPadding * 2
-    a = int(x[:j])
-    b = int(x[j:])
-    c = int(y[:j])
-    d = int(y[j:])
+    paddingB = n - j
+    paddingA = paddingB * 2
+    a = int(x[:j]) #assigns a with first half of x
+    b = int(x[j:]) #assigns b with last half of x
+    c = int(y[:j]) #assigns c with first half of y
+    d = int(y[j:]) #assigns c with last half of y
     #recursively calculate
     ac = karatsubaMultiplication(a, c)
     bd = karatsubaMultiplication(b, d)
     k = karatsubaMultiplication(a + b, c + d)
-    A = int(zeroPad(str(ac), AZeroPadding, False))
-    B = int(zeroPad(str(k - ac - bd), BZeroPadding, False))
+    A = int(zeroPad(str(ac), paddingA, False))
+    B = int(zeroPad(str(k - ac - bd), paddingB, False))
     return A + B + bd
 
+#this is grade school multiplication for comparison purpose
 def grade_school(x, y):
     #multiplying two integers using normalmultiplication method
     return x*y
